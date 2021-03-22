72604fc80486a87a1a455c4dc5c1d904011e95ae
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-22 23:33:20 +0900

    チャットルーム削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   devise_for :users
   resources :homes, only: [:index, :show]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
