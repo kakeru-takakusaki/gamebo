be9785c93ef4dc222382bda2e6702625347eb671
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-22 22:05:41 +0900

    messagesコントローラーのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   devise_for :users
   resources :homes, only: [:index, :show]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
