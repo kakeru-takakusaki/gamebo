e59bb838d3197f6383f3eedb03df923c12d93588
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-22 23:17:35 +0900

    messagesコントローラーを記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :set_room, only: [:index, :create]
+
   def index
     @message = Message.new
-    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
-    
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id)
   end
 
 end
