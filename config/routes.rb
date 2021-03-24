c42b8af2fdf5f795d468ec809b697ba81b05ebbe
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-24 22:17:39 +0900

    ルーティングにusers#showを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:show, :edit, :update]
 end
