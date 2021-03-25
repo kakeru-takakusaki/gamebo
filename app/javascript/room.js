82c9a6289cb37054ffce570cc44ef9616b169a8e
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-26 00:16:37 +0900

    selectで複数のルームメンバーidをコントローラーに送ろうと思ったがturbolinksのおかげで断念した記録

---
+++ app/javascript/room.js
@@ -0,0 +1,10 @@
+// const { $ } = require("@rails/ujs");
+
+$(document).on('turbolinks:load', function () {
+  // $('#plus').on('click', function() {
+  //   const content_area = document.getElementById('select-member');
+  //   const select = content_area.cloneNode(true);
+  //   select.id = '';
+  //   $('#add-select').append(select);
+  // });
+});
