9c5b2f804d315cebfe34cff922bfafd2eebaf1ab
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-04-03 00:58:16 +0900

    jqueryでドロップダウンの挙動を定義

--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -1,6 +1,15 @@
 function pullDown(){
   const obj = document.getElementById('messages');
   obj.scrollTop = obj.scrollHeight;
+
+  $('.chat-list-ul li').hover(function(){
+    $("ul:not(:animated)", this).slideDown();
+  }, function(){
+    $("ul.drop-element",this).slideUp();
+  });
 }
 
 document.addEventListener('turbolinks:load', pullDown)
+
+// $(document).ready(function(){
+// })
