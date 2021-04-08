ab8affb957520eb6c57684cfe87a31160b0f4ab8
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-04-08 23:37:59 +0900

    factories userの定義

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.initials(number: 5)}
+    email {Faker::Internet.free_email}
+    password {'test1234'}
+    password_confirmation {password}
+    profile_text {Faker::String.random(length: 10..100)}
+  end
+end
