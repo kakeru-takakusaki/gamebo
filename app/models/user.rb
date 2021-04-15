b5411aad41059f629a0274799f162694cb6a36d0
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-04-15 22:11:34 +0900

    nicknameの一意性にエラーメッセージを指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: { message: "can't be blank" } do
-    validates :nickname,      uniqueness: true
+    validates :nickname,      uniqueness: { message: "has already been taken"}
   end
 
   has_many :room_users
